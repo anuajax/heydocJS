61cecb371f3537e7188465d05559c448efe1a660
Ritika Mor <mor.ritika09@gmail.com>
2020-10-12 15:19:58 +0530

    Modified design , added my appointment ,patient page

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+var bodyParser=require("body-parser");
 const app = express();
 const port = 3000 || process.env.PORT;
 app.set("view engine","ejs");
 app.listen(3000, ()=>console.log(`Server active on port No: ${port}`));
 app.use(express.static(__dirname + "/styles"));
 const authRoutes = require("./routes/auth");
+app.use(bodyParser.urlencoded({extended: true}));
 app.use("/auth",authRoutes);
 app.get('/',(req,res)=>{
     res.render("landing.ejs");
--- routes/auth.js
+++ routes/auth.js
@@ -13,7 +13,17 @@ router.get("/signup/doctor",(req,res)=> {
 router.get("/signup/patient",(req,res)=>{
     res.render("patsignup.ejs")
 });
-router.post("/login",()=>{
+router.post("/login",(req,res)=>{
+	res.render("login.ejs")
     /*assign token and redirect to dashboard page*/
+})
+router.get("/doctor/dashboard",(req,res)=>{
+	res.render("docDashboard.ejs")
+})
+router.get("/doctor/appointments",(req,res)=>{
+	res.render("docAppointments.ejs")
+})
+router.get("/doctor/patients",(req,res)=>{
+	res.render("docPatients.ejs")
 })
 module.exports = router;
